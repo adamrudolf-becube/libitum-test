612afb4c9ca9386811d5eca5acc755ffd931296a
Adam Rudolf <rudolf.adam.a@gmail.com>
2020-05-13 14:29:35 +0200

    Implement product boxes in React

--- script/script.js
+++ script/script.js
@@ -1,14 +1,23 @@
-'use strict';
-
-const e = React.createElement;
-
-class ExampleText extends React.Component {
-  render() {
+function ProductBox(props) {
     return (
-        <p>ExampleText</p>
+        <label class="container">{props.name}
+            <input type="checkbox" checked={props.checked} disabled={props.disabled} />
+            <span class="checkmark"></span>
+        </label>
     );
-  }
 }
 
-const domContainer = document.querySelector('#root');
-ReactDOM.render(e(ExampleText), domContainer);
+function App(props) {
+    return (
+        <div id="checkbox-container">
+            <ProductBox name="Awesome bicycle" />
+            <ProductBox name="Extended warranty"  />
+            <ProductBox name="Gratis 3 months" disabled />
+        </div>
+    );
+}
+  
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
